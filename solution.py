870cc5c7a1760c73ac63c847663cd17bdc118ee8
taoky <taoky1999@gmail.com>
2019-11-12 21:02:28 +0800

    a bit faster

--- solution.py
+++ solution.py
@@ -1,4 +1,4 @@
-from database import database, Post
+from database import database, Post, Topic
 import markdown
 import lxml.html
 import progressbar
@@ -9,36 +9,64 @@ guess = Guess()
 
 stoplist = ["Objective-C", "Markdown", "HTML", "Erlang", "Perl", "CSS"]
 keyword_dict = OrderedDict({
-        'kotlin': 'Kotlin',
-        'python': 'Python',
-        'rust': 'Rust',
-        'scala': 'Scala',
-        'swift': 'Swift',
-        'javascript': 'JavaScript',
-        'java': 'Java',
-        'ruby': 'Ruby',
-        'php': 'PHP',
-        'sql': 'SQL',
-        'c#': 'C#',
-        'c++': 'C++',
-        'go': 'Go',
-        'sh': 'Bash',
-        'py': 'Python',
-        'js': 'JavaScript',
-        'c': 'C'
-    })
+    'kotlin': 'Kotlin',
+    'python': 'Python',
+    'rust': 'Rust',
+    'scala': 'Scala',
+    'swift': 'Swift',
+    'javascript': 'JavaScript',
+    'java': 'Java',
+    'ruby': 'Ruby',
+    'php': 'PHP',
+    'sql': 'SQL',
+    'c#': 'C#',
+    'c++': 'C++',
+    'cpp': 'C++',
+    'golang': 'Go',
+    'go': 'Go',
+    'sh': 'Bash',
+    'shell': 'Bash',
+    'bash': 'Bash',
+    'py': 'Python',
+    'js': 'JavaScript',
+    'c': 'C'
+})
+
 
 def get_from_language_keyword(content):
     content = content.lower()
-    
+
     for keyword in keyword_dict:
-        if keyword in content:
+        if keyword in content.split(" "):
             return keyword_dict[keyword]
     return None
 
+
+def pure_guess(element):
+    # returns language
+    guess_dict = guess.scores(element.text)
+    guess_dict = OrderedDict(sorted(guess_dict.items(), key=lambda x: x[1], reverse=True))
+    # print(j, guess_dict)
+
+    guess_result = list(guess_dict.items())[0]
+    guess_index = 0
+    while guess_result[0] in stoplist:
+        guess_index += 1
+        guess_result = list(guess_dict.items())[guess_index]
+
+    language = guess_result[0]
+    if guess_result[1] < 0.9:
+        # not so confident
+        key_lang = get_from_language_keyword(element.text)
+        if key_lang:
+            language = key_lang
+    return language
+
+
 def main():
     cnt = 0
-    for i in progressbar.progressbar(database.session.query(Post).filter_by(solution_language=None).all()):
+    workset = database.session.query(Post).filter(Post.solution_code.is_(None)).all()
+    for i in progressbar.progressbar(workset):
         content = markdown.markdown(
             i.content.encode('utf-8').decode('unicode-escape'),
             extensions=['fenced_code']
@@ -55,7 +83,7 @@ def main():
                     continue
                 if max_len_element.text is None or len(j.text) > len(max_len_element.text):
                     max_len_element = j
-            
+
             if max_len_element.text is None:
                 # wtf?
                 print("Post %d meets a strange error." % i.id)
@@ -68,26 +96,29 @@ def main():
                         klass = keyword_dict[klass.lower()]
                     language = klass
                     break
+            elif database.session.query(Topic).filter_by(post=i.id).count():
+                topic = database.session.query(Topic).filter_by(post=i.id).one()
+                tags = topic.tags[1:-1].split(",")
+                language = None
+                for j in tags:
+                    l = get_from_language_keyword(j)
+                    if l:
+                        language = l
+                        break
+                if not language:
+                    title = topic.title.split(" ")
+                    for j in title:
+                        l = get_from_language_keyword(j)
+                        if l:
+                            language = l
+                            break
+                if not language:
+                    language = pure_guess(max_len_element)
             else:
-                guess_dict = guess.scores(max_len_element.text)
-                guess_dict = OrderedDict(sorted(guess_dict.items(), key=lambda x: x[1], reverse=True))
-                # print(j, guess_dict)
-
-                guess_result = list(guess_dict.items())[0]
-                guess_index = 0
-                while guess_result[0] in stoplist:
-                    guess_index += 1
-                    guess_result = list(guess_dict.items())[guess_index]
-                
-                language = guess_result[0]
-                if guess_result[1] < 0.9:
-                    # not so confidient
-                    key_lang = get_from_language_keyword(max_len_element.text)
-                    if key_lang:
-                        language = key_lang
+                language = pure_guess(max_len_element)
             # print(j.text, language)
             i.solution_language = language
-            i.solution_code = j.text
+            i.solution_code = max_len_element.text
             cnt += 1
             if cnt % 100 == 0:
                 database.session.commit()
